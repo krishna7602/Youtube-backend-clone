01b9711882cbf5fea98fff657e1b63fcce5ab204
ramkrishna mondal <ramkrishnam170@gmail.com>
2025-08-07 16:13:15 +0530

    add deleteVideo function to remove a video by videoId with authorization checks

--- src/controller/Video.controller.js
+++ src/controller/Video.controller.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import {Video} from "../models/videos.models.js"
+import { error } from "console";
 
 
 
@@ -8,8 +9,8 @@ const getVideoId=async(req,res)=>{
         const {videoId}=req.params;
         const userId=req.user?._id;
         if (!userId) {
-                return res.status(401).json({ error: "Unauthorized user" });
-            }
+            return res.status(401).json({ error: "Unauthorized user" });
+        }
     
         if (!videoId) {
             return res.status(400).json({ error: "videoId is required" });
@@ -30,4 +31,34 @@ const getVideoId=async(req,res)=>{
 }
 
 
-export {getVideoId}
+const deleteVideo=async(req,res)=>{
+   try {
+     const {videoId}=req.params
+     const userId=req.user?._id
+ 
+     if(!userId){
+        return res.status(401).json({ error: "Unauthorized user" });    
+     }
+ 
+     if(!videoId){
+        return res.status(402).json({error:"error in video file!! try again"})
+     }
+ 
+     const deletedVideo=await Video.findByIdAndDelete(
+        videoId
+     )
+
+     if(!deletedVideo){
+        throw new Error("video can't found")
+     }
+ 
+     return res.status(200).json({
+        message:"video succesfully deleted"
+     })
+   } catch (error) {
+    console.log(error)
+        return res.status(500).json({ error: error.message });
+   }
+}
+
+export {getVideoId, deleteVideo}
