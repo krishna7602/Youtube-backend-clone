71c205fb7b5c73589a77d411fd975e491711e207
ramkrishna mondal <ramkrishnam170@gmail.com>
2025-08-07 16:28:03 +0530

    add updateVideo function to allow users to update video details with authorization checks

--- src/controller/Video.controller.js
+++ src/controller/Video.controller.js
@@ -61,4 +61,54 @@ const deleteVideo=async(req,res)=>{
    }
 }
 
-export {getVideoId, deleteVideo}
+
+const updateVideo = async (req, res) => {
+  try {
+    const { videoId } = req.params;
+    const userId = req.user?._id;
+
+    if (!userId) {
+      return res.status(401).json({ error: "Unauthorized user" });
+    }
+
+    if (!videoId) {
+      return res.status(400).json({ error: "Video ID is required" });
+    }
+
+    const { thumbnail, title, description } = req.body;
+
+    if (!thumbnail || !title || !description) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+   
+    const video = await Video.findById(videoId);
+    if (!video) {
+      return res.status(404).json({ error: "Video not found" });
+    }
+
+    if (video.owner.toString() !== userId.toString()) {
+      return res.status(403).json({ error: "You are not allowed to update this video" });
+    }
+
+
+    const updatedVideo = await Video.findByIdAndUpdate(
+      videoId,
+      { thumbnail, title, description },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json({
+      message: "Video successfully updated",
+      video: updatedVideo,
+    });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+
+
+export {getVideoId, deleteVideo, updateVideo}
